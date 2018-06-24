52c5035a95749761ccea0c6f23046d24f602d9c3
Igor <igor06brw@gmail.com>
2018-06-24 02:22:31 -0700

    First steps with task list
    * Init project
    * Learning modules/compositions
    * Learning directives
    * Learning databingind,

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  constructor() {}
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+  }
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter( e => e !==  task);
+
+  }
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+}
